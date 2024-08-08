4b8021cbda1c6b0a5ed27f6ee27a485d45f9762a
Andrew Buchkovskiy <andreybuchkovskiy@gmail.com>
2024-08-08 23:11:47 +0200

    add Chart

---
+++ heyAlter/StatisticVC/statisticViewController.swift
@@ -0,0 +1,29 @@
+//
+//  statisticViewController.swift
+//  heyAlter
+//
+//  Created by Andrii Buchkivskyi on 08.08.2024.
+//
+
+import UIKit
+
+class statisticViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
